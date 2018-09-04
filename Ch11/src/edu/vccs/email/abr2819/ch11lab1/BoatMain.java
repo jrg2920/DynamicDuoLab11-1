4bda2604b625a6e7f2cb6179132578a33181b724
abr2819 <42814633+abr2819@users.noreply.github.com>
2018-09-04 10:59:07 -0400

    Update BoatMain.java

--- Ch11/src/edu/vccs/email/abr2819/ch11lab1/BoatMain.java
+++ Ch11/src/edu/vccs/email/abr2819/ch11lab1/BoatMain.java
@@ -10,22 +10,21 @@ public class BoatMain {
    public static void main(String[] args) {
       Boat[] boats = new Boat[3];            // array of Boat objects (all sub-class objects)
 
-      // create 3 objects
+      // creating 3 boat objects of various types within the boats array
       boats[0] = new PowerBoat();
       boats[1] = new SUP();
       boats[2] = new PWC();
 
-      // set all the attributes
+      // sets all the attributes for created ArrayList "boats" objects
       boats[0].setLength(17.0);
       ((PowerBoat)boats[0]).setName("Lucky");
       ((PowerBoat)boats[0]).setHP(65.0);
-
       boats[1].setLength(10.4);
       ((SUP)boats[1]).IsInflatable=false;
-
       boats[2].setLength(11.1);
       ((PWC)boats[2]).setHP(200.0);
 
+      // enhanced for loop to print boat engine hp for all boat objects
       for (Boat boat : boats) {
          System.out.println(boat);
          if (boat instanceof BoatWithEngine) {
