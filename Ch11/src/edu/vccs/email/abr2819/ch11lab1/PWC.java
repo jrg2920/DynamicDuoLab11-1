975da9aae125d39ad9fb970f32c5bffccc9404ec
abr2819 <42814633+abr2819@users.noreply.github.com>
2018-09-04 11:07:37 -0400

    Update PWC.java

--- Ch11/src/edu/vccs/email/abr2819/ch11lab1/PWC.java
+++ Ch11/src/edu/vccs/email/abr2819/ch11lab1/PWC.java
@@ -1,23 +1,33 @@
 package edu.vccs.email.abr2819.ch11lab1;
 
 /**
- *
+ * gets and sets hp for a PWC boat
+ * prints string statement based on values from object creation
  * 2018-09-05
  * @author Brad Ridder
  * @author Jhanavi Ghadia
  */
 public class PWC extends Boat implements BoatWithEngine{
-   private double HP;
+   private double HP;     // hp value for a PWC boat
 
+   /**
+   * gets the value for PWC boat hp
+   */
    public double getHP() {
       return HP;
    }
 
+   /**
+   * sets the value for PWC boat hp
+   */
    @Override
    public void setHP(double HP) {
       this.HP = HP;
    }
 
+   /**
+   * prints string with length of PWC boat
+   */
    @Override
    public String toString() {
       return "This is a " + getLength() +
