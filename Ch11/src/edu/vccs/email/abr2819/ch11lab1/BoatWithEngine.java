e779e2799dfa5e62be38879ff7d276a0a79b4a00
abr2819 <42814633+abr2819@users.noreply.github.com>
2018-09-04 11:03:58 -0400

    Update BoatWithEngine.java

--- Ch11/src/edu/vccs/email/abr2819/ch11lab1/BoatWithEngine.java
+++ Ch11/src/edu/vccs/email/abr2819/ch11lab1/BoatWithEngine.java
@@ -1,13 +1,21 @@
 package edu.vccs.email.abr2819.ch11lab1;
 
 /**
- *
+ * interface for getting and setting boat hp for powerboats and PWCs
  * 2018-09-05
  * @author Brad Ridder
  * @author Jhanavi Ghadia
  */
 public interface BoatWithEngine {
+   
+   /**
+   * method header for getting boat hp
+   */
    double getHP();
+   
+   /**
+   * method header for setting boat hp
+   */
    void setHP(double HP);
 }
 
