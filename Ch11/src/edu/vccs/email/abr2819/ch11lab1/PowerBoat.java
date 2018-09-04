b566995187569627101311715c4f9a0bac995e99
abr2819 <42814633+abr2819@users.noreply.github.com>
2018-09-04 11:14:02 -0400

    Update PowerBoat.java

--- Ch11/src/edu/vccs/email/abr2819/ch11lab1/PowerBoat.java
+++ Ch11/src/edu/vccs/email/abr2819/ch11lab1/PowerBoat.java
@@ -1,31 +1,47 @@
 package edu.vccs.email.abr2819.ch11lab1;
 
 /**
- *
+ * gets and sets the name of the powerboat
+ * gets and sets the hp value for powerboat
  * 2018-09-05
  * @author Brad Ridder
  * @author Jhanavi Ghadia
  */
 public class PowerBoat extends Boat implements BoatWithEngine{
-   private String name;
-   private double HP;
+   private String name;    // name of powerboat
+   private double HP;      // value of powerboat hp
 
+   /**
+   * gets the string for powerboat name
+   */
+   public String getName() {
+      return name;
+   }
+   
+   /**
+   * sets the string for powerboat name
+   */
    public void setName(String name) {
       this.name = name;
    }
 
-   public String getName() {
-      return name;
-   }
-
-   public void setHP(double HP) {
-      this.HP = HP;
-   }
-
+   /**
+   * gets the value for powerboat hp
+   */
    public double getHP() {
       return HP;
    }
-
+   
+   /**
+   * sets the value for powerboat hp
+   */
+   public void setHP(double HP) {
+      this.HP = HP;
+   }
+   
+    /**
+   * prints string with length of powerboat
+   */
    @Override
    public String toString() {
       return  "\'" + name + "\'" +
